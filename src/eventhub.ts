4e07fe491c1ceccd6ce30d02ffd6f60496629ba8
zinkwu <zinkwu999@gmail.com>
2022-02-14 12:38:26 +0800

    format eventhub code

--- src/eventhub.ts
+++ src/eventhub.ts
@@ -1,31 +1,31 @@
 interface Callback {
-	(data?: unknown): void
+  (data?: unknown): void
 }
 
 interface Cache {
-	[eventName: string]: Array<Callback>
+  [eventName: string]: Array<Callback>
 }
 export default class EventHub {
-	private cache: Cache = {};
-	constructor() { }
+  private cache: Cache = {};
+  constructor() { }
 
-	on(eventName: string, callback: Callback) {
-		this.cache[eventName] = this.cache[eventName] || []
-		this.cache[eventName].push(callback)
-	}
+  on(eventName: string, callback: Callback) {
+    this.cache[eventName] = this.cache[eventName] || []
+    this.cache[eventName].push(callback)
+  }
 
-	emit(eventName: string, data?: unknown) {
-		if (!this.cache[eventName]) {
-			return
-		}
-		this.cache[eventName].forEach(fn => fn(data))
-	}
+  emit(eventName: string, data?: unknown) {
+    if (!this.cache[eventName]) {
+      return
+    }
+    this.cache[eventName].forEach(fn => fn(data))
+  }
 
-	off(eventName: string, callback: Callback) {
-		const cbList = this.cache[eventName]
-		const index = cbList.indexOf(callback)
-		if (index >= 0) {
-			cbList.splice(index, 1)
-		}
-	}
+  off(eventName: string, callback: Callback) {
+    const cbList = this.cache[eventName]
+    const index = cbList.indexOf(callback)
+    if (index >= 0) {
+      cbList.splice(index, 1)
+    }
+  }
 }
